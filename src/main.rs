6cd27d5a2275d176f3bb028dbe0090bf50ee2b26
shiyasmohd <shiyasjaseena124@gmail.com>
2024-08-11 18:58:23 +0530

    exercise 7: age on planets

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use std::{collections::HashSet, fmt, ops::Add};
+use std::{
+    collections::HashSet,
+    fmt,
+    ops::{Add, Div, Mul},
+};
 
 use time::{OffsetDateTime, PrimitiveDateTime};
 fn main() {
-    println!("{}", is_armstrong_number(4_106_098_957));
+    println!("{}", square_of_sum(10));
 }
 
 // Exercise 1
@@ -156,4 +160,88 @@ pub fn is_armstrong_number(num: i64) -> bool {
 
 fn number_of_digits(num: i64) -> u32 {
     num.to_string().len().try_into().unwrap()
+}
+
+// Excercise 6
+pub fn square_of_sum(n: u32) -> u32 {
+    (n * (n + 1)).pow(2).div_euclid(4)
+}
+
+pub fn sum_of_squares(n: u32) -> u32 {
+    (n * (n + 1) * (2 * n + 1)) / 6
+}
+
+pub fn difference(n: u32) -> u32 {
+    square_of_sum(n).abs_diff(sum_of_squares(n))
+}
+
+// Exercise 7
+#[derive(Debug)]
+pub struct Duration {
+    age_in_earth: f64,
+}
+
+impl From<i64> for Duration {
+    fn from(s: i64) -> Self {
+        Self {
+            age_in_earth: (s as f64 / 31_557_600.0).mul(100.0).round().div(100.0),
+        }
+    }
+}
+
+pub trait Planet {
+    fn years_during(d: &Duration) -> f64 {
+        d.age_in_earth / Self::orbital_period()
+    }
+    fn orbital_period() -> f64;
+}
+
+pub struct Mercury;
+pub struct Venus;
+pub struct Earth;
+pub struct Mars;
+pub struct Jupiter;
+pub struct Saturn;
+pub struct Uranus;
+pub struct Neptune;
+
+impl Planet for Mercury {
+    fn orbital_period() -> f64 {
+        0.2408467
+    }
+}
+impl Planet for Venus {
+    fn orbital_period() -> f64 {
+        0.61519726
+    }
+}
+impl Planet for Earth {
+    fn orbital_period() -> f64 {
+        1.0
+    }
+}
+impl Planet for Mars {
+    fn orbital_period() -> f64 {
+        1.8808158
+    }
+}
+impl Planet for Jupiter {
+    fn orbital_period() -> f64 {
+        11.862615
+    }
+}
+impl Planet for Saturn {
+    fn orbital_period() -> f64 {
+        29.447498
+    }
+}
+impl Planet for Uranus {
+    fn orbital_period() -> f64 {
+        84.016846
+    }
+}
+impl Planet for Neptune {
+    fn orbital_period() -> f64 {
+        164.79132
+    }
 }
